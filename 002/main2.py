a15a6990fafdc67b4d8ab9fc9290118fe94fc616
mirgjen_ <mirgjen.gashi5@gmail.com>
2026-03-15 12:30:01 +0100

    "git push"

--- 002/main2.py
+++ 002/main2.py
@@ -20,6 +20,35 @@
 # and, or, not
 
 
+# Operatoret e krahasimit (kthejn vetem nje data type, si output(booleans))
+# and, or, not
+
+print(10 > 5)
+print(10 >= 7)
+print(9 < 3)
+print(15 == 24)
+print(20 != 7)
+
+
+
+print('------------------------------')
+
+email= 'mirgjen.gashi1@gmail.com'
+password= '1234'
+
+if email == 'mirgjen.gashi1@gmail.com' and password == '1234':
+    print("Welcome, you are successfully logged in!")
+else:
+    print("Email or password is incorrect.")
+
+
+    
+
+
+
+
+
+
 
 
 
